25bf939c29016b0f776a84cbcf8caf8fd4d4d968
Alex Wahlroos <alex.wahlroos@hotmail.com>
2023-05-02 16:37:39 +0200

    Some work done on Projects

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,12 +1,42 @@
 import React from "react";
 import "../styles/Project.css";
+import { useState, useEffect } from "react";
 
 interface Props {
   title: string;
   shortDesc: string;
-  tags: string;
-  image: string;
+  tags: Array<string>;
+  images: Array<string>;
 }
-export default function Project({ title, shortDesc, tags, image }: Props) {
-  return <div id="top-container">asdasd</div>;
+
+export default function Project({ title, shortDesc, tags, images }: Props) {
+  //images.map(img => import(`/* @vite-ignore */${img}`))
+
+  return (
+    <div id="top-container">
+      <div id="project-container">
+        <div className="text-container">
+          <div>
+            <h2 className="title-text">{title}</h2>
+            <p className="description-text">{shortDesc}</p>
+          </div>
+          <div>
+            <h4 style={{ display: "inline" }}>#</h4>
+            <p style={{ display: "inline" }}>
+              {tags.map((tag) => (
+                <span className="tag">{tag}</span>
+              ))}
+            </p>
+          </div>
+        </div>
+        <div className="image-container">
+          <img src={images[0]} />
+        </div>
+        <div className="button-container">
+          <button id="circle-button"></button>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
 }
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Landing from "../components/Landing";
 import Project from "../components/Project";
+import About from "../components/About";
 import "../styles/App.css";
 import projectData from "../data/projectData.json";
 
@@ -8,8 +9,17 @@ function App() {
   return (
     <div className="App">
       <div className="home-top-container">
-        <Landing />
-        {/*<Project {...projectData} />*/}
+        <About />
+        {/*<Landing />*/}
+        {/*{projectData.map((project, index) => (
+          <Project
+            key={index}
+            title={project.title}
+            shortDesc={project.shortDesc}
+            tags={project.tags}
+            images={project.images}
+          />
+        ))}*/}
       </div>
     </div>
   );
