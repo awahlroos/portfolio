f2a9e252e85c817dedc4e839c7b324cb748e6d46
alexwahlroos <alex.wahlroos@hotmail.com>
2023-09-07 16:06:53 +0200

    Adds animation

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -11,20 +11,30 @@ export default function Project({ handleClick }: Props) {
       <div className="outer-landing-container">
         <div className="inner-landing-container">
           <div id="landing-text-container">
-            <p>Hej, mitt namn är</p>
-            <h1 id="landing-text-name">ALEX WAHLROOS</h1>
-            <p>
+            <p className="drop-in fade-in">Hej, mitt namn är</p>
+            <h1 className="drop-in-2 fade-in" id="landing-text-name">
+              ALEX WAHLROOS
+            </h1>
+            <p className="drop-in-3 fade-in">
               och jag är civilingenjörsstudent inom{" "}
               <b>interaktion och design</b>. Det här är en hemsida för att
               presentera min portfolio och ett urval av arbeten jag gjort under
               min studietid. Hoppas du hittar något du gillar!
             </p>
-            <button className="primary-button" onClick={handleClick}>
+            <button
+              className="primary-button drop-in-4 fade-in"
+              onClick={handleClick}
+            >
               Läs mer
             </button>
           </div>
           <div id="landing-image-container">
-            <img id="landing-image" src={landing_image} alt="Alex Wahlroos" />
+            <img
+              id="landing-image"
+              className="drop-in fade-in"
+              src={landing_image}
+              alt="Alex Wahlroos"
+            />
           </div>
         </div>
       </div>
