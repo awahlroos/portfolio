795af7579b0e5c9ebec02373a23959bed28a04bd
Alex Wahlroos <alex.wahlroos@hotmail.com>
2023-09-09 13:04:23 +0200

    Adds swiper, various changes

--- src/pages/ProjectPage.tsx
+++ src/pages/ProjectPage.tsx
@@ -1,5 +1,10 @@
 import { useParams } from "react-router-dom";
 import projectData from "../data/projectData.json";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css";
 import "../styles/ProjectPage.css";
 
 export default function ProjectPage() {
@@ -24,25 +29,67 @@ export default function ProjectPage() {
         </div>
       </div>
       <div className="home-top-container">
-        {project.background && [
-          <h3 style={{ marginTop: "10vh" }}>Bakgrund</h3>,
-          <p className="paragraph">{project.background}</p>,
-        ]}
-        {project.idea && [
-          <h3>Projektidé</h3>,
-          <p className="paragraph">{project.idea}</p>,
-        ]}
-        {project.process && [
-          <h3>Projektgenomförande</h3>,
-          <p className="paragraph">{project.process}</p>,
-        ]}
-        {project.images &&
-          project.images.length > 0 && [
-            <h3>Bilder</h3>,
-            project.images.map((image) => {
-              <img src={image} />;
-            }),
-          ]}
+        {project.background && (
+          <>
+            <h3 style={{ marginTop: "10vh" }}>Bakgrund</h3>
+            <p className="paragraph">{project.background}</p>
+          </>
+        )}
+        {project.idea && (
+          <>
+            <h3>Projektidé</h3>
+            <p className="paragraph">{project.idea}</p>
+          </>
+        )}
+        {project.process && (
+          <>
+            <h3>Projektgenomförande</h3>
+            <p className="paragraph">{project.process}</p>
+          </>
+        )}
+        {project.images && project.images.length > 0 && (
+          <>
+            <h3>Bilder</h3>
+            <Swiper
+              modules={[Navigation, Pagination]}
+              slidesPerView={1}
+              centeredSlides={true}
+              loop={true}
+              navigation={true}
+              pagination={{
+                el: ".swiper-custom-pagination",
+                clickable: true,
+                renderBullet: function (index, className) {
+                  return `<div className=${className}>
+                  <span className="number">${index + 1}</span>
+                  <span className="line"/>
+                  </div>`;
+                },
+              }}
+              breakpoints={
+                {
+                  800: {
+                    slidesPerView: 1.25
+                  }
+                }
+              }
+            >
+              {project.images.map((image, index) => (
+                <SwiperSlide className="swiper-slide">
+                  <figure>
+                    <img
+                      src={`/images/${image}.png`}
+                      alt={`Project Image ${index + 1}`}
+                      style={{ width: "100%", height: "100%" }}
+                    />
+                    <figcaption><div><p>{project.imageCaption[index]}</p><h5>{project.imageSubCaption[index]}</h5></div></figcaption>
+                  </figure>
+                </SwiperSlide>
+              ))}
+              <div className="swiper-custom-pagination"></div>
+            </Swiper>
+          </>
+        )}
       </div>
     </>
   );
