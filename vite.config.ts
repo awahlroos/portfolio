54db04947b22e3cdb8caf7101b487aae887da670
Alex Wahlroos <alex.wahlroos@hotmail.com>
2024-06-06 17:09:27 +0200

    troubleshooting

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   base: "/portfolio/",
   build: {
     rollupOptions: {
@@ -28,3 +28,6 @@ export default defineConfig({
     },
   },
 });
+function tsconfigPaths(): import("vite").PluginOption {
+  throw new Error("Function not implemented.");
+}
