d4d0aeab76c19102bde74514a15805c9f30590bd
Alex Wahlroos <alex.wahlroos@hotmail.com>
2024-06-06 16:16:09 +0200

    Updated education status

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -18,9 +18,9 @@ export default function About({ innerRef }: Props) {
         <div id="about-me-container">
           <img src={about_me_image} id="about-me-image" alt="Image of me" />
           <p id="about-me-text">
-            Jag heter Alex Wahlroos och studerar femte och sista året på
+            Jag heter Alex Wahlroos och har precis studerat klart
             civilingenjörsprogrammet interaktion och design vid Umeå
-            Universitet. En vanlig beskrivning av detta program är att{" "}
+            universitet. En vanlig beskrivning av detta program är att{" "}
             <b>mötet mellan människa och maskin</b> står i fokus, vilket i
             praktiken innebär att designa och skapa intuitiva och
             användarvänliga gränssnitt och system.{" "}
